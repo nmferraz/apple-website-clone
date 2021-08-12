35a26b6831149a6b83e74e96dff342609db9a0b0
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2021-08-12 16:06:11 +0100

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -1,4 +1,9 @@
 //JavaScript
 
+const selectElement= (element) => document.querySelector(element);
+
+selectElement('.mobile-menu').addEventListener('click', () => {
+    selectElement('header').classList.toggle('active');
+})
 
 ////// Develop by nmferraz, for education purpose //////
